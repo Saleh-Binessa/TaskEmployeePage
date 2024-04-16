2a88ac87937480661d962e1053bffcc85fcd8e43
Saleh-Binessa <saleh.binessa@gmail.com>
2024-04-16 14:25:11 +0300

    Add project files.

---
+++ TaskEmployeePage/Controllers/EmployeeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskEmployeePage.Models;
+
+namespace TaskEmployeePage.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var employees = new List<Employee>
+    {
+        new Employee { EmployeeId = 1, FirstName = "Saleh", LastName = "Bin Essa", Email = "saleh.binessa@gmail.com", Department = "IT"},
+        new Employee { EmployeeId = 2, FirstName = "Nawaf", LastName = "Almutairi", Email = "nawaf.almutairi@gmail.com", Department = "IT"},
+    };
+
+            return View(employees);
+        }
+    }
+}
---
+++ TaskEmployeePage/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TaskEmployeePage.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
